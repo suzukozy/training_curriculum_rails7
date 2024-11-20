a51468855d76a68b54bc682f919b49d8ad78ece5
suzukozy <suzukozy0405@yahoo.co.jp>
2024-11-20 17:12:58 +0900

    Revert "Merge pull request #8 from suzukozy/second_training"
    This reverts commit 452811ab235589e05c3c70f8971ea06a573e3406, reversing
    changes made to 7301fb45707eda9680bc8153f489e1d7ca134f8c.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,8 +1,8 @@
 class CalendarsController < ApplicationController
 
-  # 1週間のカレンダーと予定が表示されるページ
+  # １週間のカレンダーと予定が表示されるページ
   def index
-    get_week
+    getWeek
     @plan = Plan.new
   end
 
@@ -15,28 +15,28 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
-  def get_week
-    wdays = ['(日)', '(月)', '(火)', '(水)', '(木)', '(金)', '(土)']
-  
+  def getWeek
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
+    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+
     @week_days = []
-  
-    plans = Plan.where(date: @todays_date..@todays_date + 6).group_by(&:date)
-  
+
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
+
     7.times do |x|
-      target_date = @todays_date + x
-      day_plans = plans[target_date]&.map(&:plan) || []
-  
-      wday_num = (target_date.wday) % 7
-      @week_days << {
-        month: target_date.month,
-        date: target_date.day,
-        wday: wdays[wday_num], 
-        plans: day_plans
-      }
+      today_plans = []
+      plans.each do |plan|
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      end
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      @week_days.push(days)
     end
+
   end
 end
